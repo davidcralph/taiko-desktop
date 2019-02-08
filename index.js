5a663da4fa3a3cbd4ac69e584f814f9b9857b4ca
David Ralph <ohlookitsderpy@protonmail.com>
2019-02-08 22:40:50 +0000

    Add config

--- index.js
+++ index.js
@@ -1,73 +1,87 @@
-//Declarations
+// Declarations
 const util                                                     = require('./modules/util');
 const { join }                                                 = require('path');
 const DiscordRPC                                               = require('./modules/rpc');
+const { existsSync, writeFileSync }                            = require('fs');
 const { app, BrowserWindow, ipcMain, Menu, Tray, nativeImage } = require('electron');
 
+// Config
+const homedir = require('os').homedir();
+const sample = { 
+  "url": "https://taiko.derpyenterprises.org",
+  "rpc": true,
+  "tray": true
+}
+if (!existsSync(`${homedir}/taikoconfig.json`)) writeFileSync(`${homedir}/taikoconfig.json`, JSON.stringify(sample));
+const config = require(`${homedir}/taikoconfig.json`);
 
-//Electron
+// Electron
 let win;
 
 app.on('ready', () => {
-  win = new BrowserWindow({height: 600, width: 800 });
+  win = new BrowserWindow({height: 600, width: 800});
   win.loadFile('./public/index.html');
-  
-  let icon = nativeImage.createFromPath(join(__dirname, 'public', 'icon.png'));
-  let tray = new Tray(icon);
-  let contextMenu = Menu.buildFromTemplate([{
-      label: 'Show',
-      click: () => win.show()
-    },
-    {
-      label: 'Hide',
-      click: () => win.close()
-    },
-    {
-      label: 'Quit',
-      click: () => {
-        app.isQuiting = true;
-        tray.destroy();
-        process.exit(3);
-      }
-    },
-  ]);
-  tray.setContextMenu(contextMenu);
-  tray.setToolTip('Taiko Desktop');
+
+  if (config.tray = true) {
+    let icon = nativeImage.createFromPath(join(__dirname, 'public', 'icon.png'));
+    let tray = new Tray(icon);
+    let contextMenu = Menu.buildFromTemplate([{
+        label: 'Show',
+        click: () => win.show()
+      },
+      {
+        label: 'Hide',
+        click: () => win.close()
+      },
+      {
+        label: 'Quit',
+        click: () => {
+          app.isQuiting = true;
+          tray.destroy();
+          process.exit(3);
+        }
+      },
+    ]);
+    tray.setContextMenu(contextMenu);
+    tray.setToolTip('Taiko Desktop');
+  }
 });
 
 
-//Discord RPC
-const timestamp = new Date().getTime();
+// Discord RPC
+if (config.rpc = true) {
+  const timestamp = new Date().getTime();
 
-const uRPC = new DiscordRPC({ clientID: '536293982209310730', debug: false }); //debug gang, lets not debug for now
+  const uRPC = new DiscordRPC({clientID: '536293982209310730', debug: false}); //debug gang, lets not debug for now
 
-const setActivity = async () => {
-      ipcMain.on('RpcToSongSelect', () => {
-        uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'Choosing a song'));
-      });
+  const setActivity = async () => {
+    ipcMain.on('RpcToSongSelect', () => {
+      uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'Choosing a song'));
+    });
 
-      ipcMain.on('RpcToMainMenu', () => {
-        uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'On the Main Menu'));
-      });
+    ipcMain.on('RpcToMainMenu', () => {
+      uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'On the Main Menu'));
+    });
 
-      ipcMain.on('RpcToGame', (info, data) => {
-        uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, data.songname, data.difficulty));
-      });
+    ipcMain.on('RpcToGame', (info, data) => {
+      uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, data.songname, data.difficulty));
+    });
 
-      ipcMain.on('RpcToLoading', () => {
-        uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'Loading...'));
-      });
+    ipcMain.on('RpcToLoading', () => {
+      uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'Loading...'));
+    });
 
-      ipcMain.on('RpcToPaused', () => {
-        uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'Game Paused'));
-      });
+    ipcMain.on('RpcToPaused', () => {
+      uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'Game Paused'));
+    });
 
-      ipcMain.on('RpcToMultiplayer', () => {
-        uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'In Multiplayer'));
-      });
-};
+    ipcMain.on('RpcToMultiplayer', () => {
+      uRPC.send('SET_ACTIVITY', util.rpcStatus(timestamp, 'In Multiplayer'));
+    });
+  };
 
-uRPC.on('ready', () => {
-  setActivity();
-  setInterval(setActivity, 15e3);
-});
+  uRPC.on('ready', () => {
+    setActivity();
+    setInterval(setActivity, 15e3);
+  });
+}
